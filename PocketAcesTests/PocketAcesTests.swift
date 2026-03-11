6fed34cedc12ce33ed010f7a92ee69ac448db334
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2026-03-11 12:42:44 -0700

    Initial Commit

---
+++ PocketAcesTests/PocketAcesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketAcesTests.swift
+//  PocketAcesTests
+//
+//  Created by Rishi Murumkar on 3/11/26.
+//
+
+import Testing
+@testable import PocketAces
+
+struct PocketAcesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
